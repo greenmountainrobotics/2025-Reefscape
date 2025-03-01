ca9c9b3ed621629618960d4138c9f345c577c9f4
no_not_Epsilon <alyssahill@cvsdvt.org>
2025-03-01 13:38:06 -0500

    Intake Simulation Done!

--- src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
@@ -1,3 +1,70 @@
 package frc.robot.subsystems.intake;
 
-public class IntakeIOSim {}
+
+//import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+
+public class IntakeIOSim implements IntakeIO {
+    private static final double LOOP_PERIOD_SECS = 0.02;
+
+    private final DCMotor intakeSpinMotor = DCMotor.getVex775Pro(1);
+    private final DCMotor intakeRotateMotor = DCMotor.getNEO(1);
+
+    private DCMotorSim intakeSpinMotorSim = 
+    new DCMotorSim(
+        LinearSystemId.createDCMotorSystem(
+            intakeSpinMotor, 1.0, 100),
+            intakeSpinMotor);
+    private DCMotorSim intakeRotateMotorSim =
+    new DCMotorSim(
+        LinearSystemId.createDCMotorSystem(
+            intakeRotateMotor, 1.0, 80),
+            intakeRotateMotor);
+
+    private double spinAppliedVolts = 0.0;
+    private double spinCurrentAmps = 0.0;
+    
+    private double articulationAppliedVolts = 0.0;
+    private double articulationCurrentAmps = 0.0;
+
+    @Override
+    public void updateInputs(IntakeIOInputs inputs) {
+        intakeSpinMotorSim.update(LOOP_PERIOD_SECS);
+        intakeRotateMotorSim.update(LOOP_PERIOD_SECS);
+
+        //inputs.articulationPosition  = intakeSpinMotor.();
+        /*
+        private double intakeSpinMotorVelocityRadPerSec = intakeSpinMotor.getAngularVelocityRadPerSec();
+        private double intakeRotateMotorPositionRad = intakeRotateMotor.getAngularPositionRad();
+        private double intakeRotateMotorVelocityRadPerSec = intakeRotateMotor.getAngularVelocityRadPerSec();
+        */
+        inputs.spinAppliedVolts = spinAppliedVolts;
+        inputs.spinCurrentAmps = spinCurrentAmps;
+        
+        inputs.articulationAppliedVolts = articulationAppliedVolts;
+        inputs.articulationCurrentAmps = articulationCurrentAmps;
+
+        inputs.spinAppliedVolts = intakeSpinMotorSim.getInputVoltage();
+        inputs.spinCurrentAmps = intakeSpinMotorSim.getCurrentDrawAmps();
+
+        inputs.spinPositionRad = intakeSpinMotorSim.getAngularPositionRad();
+        inputs.spinAngularVelocity = intakeSpinMotorSim.getAngularVelocity();
+
+        inputs.articulationPositionRad = intakeRotateMotorSim.getAngularPositionRad();
+        inputs.articulationAngularVelocity = intakeSpinMotorSim.getAngularVelocity();
+}
+
+    @Override
+    public void articulationRunVoltage(double voltage) {
+        articulationAppliedVolts = voltage;
+        intakeRotateMotorSim.setInputVoltage(voltage); //why setINPUTVoltage?
+    }
+
+    @Override
+    public void spinRunVoltage(double voltage) {
+        spinAppliedVolts = voltage;
+        intakeSpinMotorSim.setInputVoltage(voltage);  //why setINPUTVoltage?
+    }
+}
