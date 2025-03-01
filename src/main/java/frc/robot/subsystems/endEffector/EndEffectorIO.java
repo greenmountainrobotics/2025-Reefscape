18fcf6af73667d4f6e246ebdd8519da673cda7dd
no_not_Epsilon <alyssahill@cvsdvt.org>
2025-03-01 13:02:06 -0500

    ENDEFFECTOR SIM NO ERRORS!!!!!!!!

--- src/main/java/frc/robot/subsystems/endEffector/EndEffectorIO.java
+++ src/main/java/frc/robot/subsystems/endEffector/EndEffectorIO.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems.endEffector;
 
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.units.measure.AngularVelocity;
+
 import org.littletonrobotics.junction.AutoLog;
 
 public interface EndEffectorIO {
@@ -14,6 +16,14 @@ public interface EndEffectorIO {
     public double spinAppliedVolts = 0.0;
     public double spinCurrentAmps = 0.0;
     public boolean limitSwitchPressed = false;
+
+    public double spinPositionRad = 0.0;
+    public AngularVelocity spinAngularVelocity = 0.0;
+
+    public AngularVelocity articulationPositionRad = 0.0;
+    public double articulationAngularVelocity = 0.0;
+
+
   }
 
   default void updateInputs(EndEffectorIOInputs inputs) {}
